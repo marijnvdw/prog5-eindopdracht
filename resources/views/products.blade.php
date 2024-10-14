5b89f8dff086eda027453232ba59676f39c8a34e
marijnvdw <m.vdwaterlaat@gmail.com>
2024-10-14 10:59:58 +0200

    Changed Readme and added about us page

---
+++ resources/views/products.blade.php
@@ -0,0 +1,2 @@
+<h1>Product ID: {{$id}}</h1>
+<a href="{{route('contact', ['company' => 'hi'])}}" >Link naar product page</a>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,38 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutUsController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/contact', function() {
+    $company = 'Hogeschool Rotterdam';
+        return view('contact', [
+    'company' => $company
+    ]);
+})->name('contact');
+
+//Route::get('products/{id}', function(int $id) {
+//    return view('products', ['id' => $id]);
+//})->name('products');
+Route::resource('products', ProductController::class);
+
+
+Route::get('about-us', [AboutUsController::class, 'Show'])->name('about-us');
+
+
+require __DIR__.'/auth.php';
